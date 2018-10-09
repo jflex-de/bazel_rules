564b3e55eb8338632c1662688f0d059033ea0371
Régis Décamps <regisd@google.com>
2018-10-09 11:40:11 +0200

    Let "hello world" run the lexer on test data.
    N.B. This is only testing that the lexer returns no error code.

--- javatests/jflex/examples/helloworld/HelloWorldTest.java
+++ javatests/jflex/examples/helloworld/HelloWorldTest.java
@@ -6,6 +6,12 @@ import org.junit.Test;
  * Test for {@link HelloWorld} lexer.
  */
 public class HelloWorldTest {
+
+  /**
+   * Integration test for the lexer.
+   */
   @Test
-  public void test() {}
+  public void testLexer() {
+    HelloWorld.main(new String[]{"jflex/examples/helloworld/testdata.txt"});
+  }
 }
